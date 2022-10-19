c69e062bf773aaaf968e6a42ab2e77b8aa5e2478
Anddd7 <liaoad_space@sina.com>
2022-10-19 14:30:26 +0800

    add argocd terraform

--- terraform/minikube/dev/main.tf
+++ terraform/minikube/dev/main.tf
@@ -10,3 +10,18 @@ module "mysql" {
   namespace = var.namespace
 }
 
+resource "kubernetes_config_map" "ratings" {
+  metadata {
+    name      = "ratings-terraform"
+    namespace = var.namespace
+  }
+
+  data = {
+    DB_TYPE           = module.mysql.db_type
+    MYSQL_DB_HOST     = module.mysql.mysql_db_host
+    MYSQL_DB_PORT     = module.mysql.mysql_db_port
+    MYSQL_DB_USER     = module.mysql.mysql_db_user
+    MYSQL_DB_PASSWORD = module.mysql.mysql_db_password
+    # MONGO_DB_URL      = "mongodb://mongodb:27017/test"
+  }
+}
---
+++ terraform/minikube/shared/terraform.tfvars
@@ -0,0 +1,1 @@
+platform = "minikube"
