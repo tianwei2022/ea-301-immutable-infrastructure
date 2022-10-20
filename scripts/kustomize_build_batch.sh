32116096978c6321224690c01299a495c099810f
Anddd7 <liaoad_space@sina.com>
2022-10-20 17:04:16 +0800

    add missing env

--- scripts/kustomize_build_batch.sh
+++ scripts/kustomize_build_batch.sh
@@ -1,15 +1,33 @@
-service=(
-  'details'
-  'productpage'
-  'ratings'
-  'reviews'
- )
+#!/usr/bin/env bash
+
+VAR_GIT_ROOT_DIR=$(git rev-parse --show-toplevel)
+
+SERVICE_NAME=$1
+OVERLAY_NAME="$2-$3"
+IMAGE_NAME=$4
+
+OVERLAY_PATH="$VAR_GIT_ROOT_DIR/apps/$SERVICE_NAME/k8s/overlays/$OVERLAY_NAME"
+MANIFEST_PATH="$VAR_GIT_ROOT_DIR/build/$OVERLAY_NAME/$SERVICE_NAME"
+
+mkdir -p "$MANIFEST_PATH"
+
+export SERVICE_NAME=$SERVICE_NAME
+export OVERLAY_NAME=$OVERLAY_NAME
+export IMAGE_NAME=$IMAGE_NAME
+
+kustomize build "$OVERLAY_PATH" | envsubst >| "$MANIFEST_PATH"/k8s.yaml
+
+#!/usr/bin/env bash
+
+service=('details' 'productpage' 'ratings' 'reviews')
 images=(
-  ${{ needs.build-and-push-image.outputs.details }}
-  ${{ needs.build-and-push-image.outputs.productpage }}
-  ${{ needs.build-and-push-image.outputs.ratings }}
-  ${{ needs.build-and-push-image.outputs.reviews }}
+  'test'
+  'test'
+  'test'
+  'test'
 )
+platform=('minikube' 'colima')
+env=('dev')
 
 for i in "${!service[@]}"; do
   s=${service[i]}
@@ -17,7 +35,7 @@ for i in "${!service[@]}"; do
 
   for p in "${platform[@]}"; do
     for e in "${env[@]}"; do
-      ./scripts/kustomize_build_ci.sh "$s" "$p" "$e" "$image"
+      echo "$s" "$p" "$e" "$image"
     done
   done
 done
